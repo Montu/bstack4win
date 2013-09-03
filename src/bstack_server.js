cebf550265d9b46f9c02bdf1fccf6e2797aa8b9b
Akshay Bhardwaj <akshay@browserstack.com>
2013-09-04 00:21:08 +0530

    Cleaned up bstack_server.js

--- src/bstack_server.js
+++ src/bstack_server.js
@@ -1,15 +1,9 @@
 /* Usage: 
- * First call npm install to install dependencies
- * To start first install node.js on your computer and then run in shell : node bstact_server.js
- * In browser type: http://127.0.0.1:8081/firefox/start to start firefox and /firefox/close to close it
- * and completely delete the data. Support for Safari Added.
- * ** This could be more cleaned up by making a routes like config file which at
- * start of application is loaded on a mapping hash
- * **** Thoroughly tested for Firefox. Stop command partially tested for Chrome and Safari
- * Stop by default clears the entier browser data and makes it a fresh browser ready to run 
- * next time. 
+ * @Author : Akshay Bhardwaj
+ * @Usage: Install node.js on your computer and run the batch file placed in project's root folder.
  */
 
+// Variable declaration starts
 var sys = require("sys"),
 my_http = require("http"),
 path = require("path"),
@@ -19,11 +13,10 @@ fsex = require("fs-extra"),
 exec = require('child_process').exec,
 plist = require('plist'),
 global = require('../global.js'),
-browser_location = "",
-browser_name = "",
-browser_data_location = "",
 _supported_browsers = ['firefox','chrome','iexplore','opera']
+// Variable declaration ends
 
+// Helper Function Definitions start (This could be moved to _helper.js for clearity, which will be done in next iteration
 var isEmptyObject = function(obj) {
 
 	console.log("INside isEmptyObject: " + Object.keys(obj).length)
@@ -70,30 +63,26 @@ var _callback_arg_func = function(error, stdout, stderr) {
 					}
 				}
 
-// console.log("Directory name " + (__dirname || "Not Found"))
+// Helper function definitions end
 
 my_http.createServer(function(request, response) {
 
 	
 	// Prepare URL data
-	//console.log("URL :" + request.url)
 	var parsed_url = url.parse(request.url, true)
-	//console.log(JSON.stringify(parsed_url))
 	var var_path = parsed_url.pathname
-	//console.log("var_path :" + var_path)
 	var query = parsed_url.query
 	var split_path = var_path.split('/')
-	//console.log(split_path)
-	//console.log(query.proxy + " " + query.address + " " + query.port + "printed\n")
+
 	
 	// Load configuration file
 	var mapping = JSON.parse(fs.readFileSync(global.project_root+"/config/browser_mapper.json"))
-	//console.log(JSON.stringify(mapping))
 	
 	var browser_key = split_path[1]
 	var browser_command = split_path[2]
 	
 	//if(isBrowserSupported(browser_key) >= 0) {
+	// Facing some weird error in isBrowserSupported implementation hence shifted to alternative implementation.
 	if(['firefox','chrome','iexplore','opera'].indexOf(browser_key) >= 0)
 	{
 		
